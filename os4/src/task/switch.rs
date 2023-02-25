5a8398a75f8af878e6df1c90c58b033e24a21f8c
Jonir Rings <i@jonirrings.com>
2023-02-25 22:47:47 +0800

    done

---
+++ os4/src/task/switch.rs
@@ -0,0 +1,6 @@
+use crate::task::TaskContext;
+core::arch::global_asm!(include_str!("switch.S"));
+
+extern "C" {
+    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
+}
